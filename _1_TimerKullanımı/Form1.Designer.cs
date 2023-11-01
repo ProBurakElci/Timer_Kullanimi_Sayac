9084046c66ae784e2c59ca1e7975b6d8ce2494b4
Burak Elçi <burakelci12@gmail.com>
2023-11-01 15:53:24 +0300

    Proje dosyası ekle.

---
+++ _1_TimerKullanımı/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _1_TimerKullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.tmrZamanlayici = new System.Windows.Forms.Timer(this.components);
+            this.lblSayaç = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(156, 238);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Location = new System.Drawing.Point(156, 282);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(75, 23);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // tmrZamanlayici
+            // 
+            this.tmrZamanlayici.Tick += new System.EventHandler(this.tmrZamanlayici_Tick);
+            // 
+            // lblSayaç
+            // 
+            this.lblSayaç.AutoSize = true;
+            this.lblSayaç.Font = new System.Drawing.Font("Cooper Black", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.lblSayaç.ForeColor = System.Drawing.Color.Black;
+            this.lblSayaç.Location = new System.Drawing.Point(121, 124);
+            this.lblSayaç.Name = "lblSayaç";
+            this.lblSayaç.Size = new System.Drawing.Size(129, 35);
+            this.lblSayaç.TabIndex = 2;
+            this.lblSayaç.Text = "SAYAÇ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(392, 345);
+            this.Controls.Add(this.lblSayaç);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Timer tmrZamanlayici;
+        private System.Windows.Forms.Label lblSayaç;
+    }
+}
+
---
+++ _1_TimerKullanımı/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_TimerKullanımı
+{
+    public partial class Form1 : Form
+    {
+        // Timer : Belirli bir süre dahilinde belirli bir iş yaptırmaya yarar.
+        // her 10 dakikada bir uyarı ver, her 5 sn de bir kontrol et, her 2 saatte bir veritabanının yedeğini al vs.
+        // Interval Özelliği : Timer ın, belirtilen işi ne kadar sürede bir yapacağını belirler. milisaniye cinsinden değer alır.
+        // Tick Olayı : Intervalde belirlenen süre her geçtiğinde, Tick olayı otomatik olarak çalışır.
+        // NOT : Timer ın çalışmaya başlaması için Start() metodu kullnaılmalıdır. Timer kendi kendine çalışmaya başlamaz.
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int sayac;
+        private void tmrZamanlayici_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            lblSayaç.Text = sayac + "";
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            //tmrZamanlayici.Start(); // timer ın çalışmaya başlamasını sağlar
+            tmrZamanlayici.Enabled = true; // böyle de başlatılabilir
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            // tmrZamanlayici.Stop(); // timer ın çalışmasını durdurur.
+            tmrZamanlayici.Enabled = false;// böyle de durdurulabilir
+        }
+    }
+}
